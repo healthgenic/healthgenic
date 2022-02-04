f63c737ffcfda0c54994f867363b09eb10e0e5e1
Ajay Shinde <ajayshinde1@protonmail.com>
2022-02-04 18:00:35 +0000

    Initial Commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -49,3 +49,6 @@ export default App;
 {/* <Route path = "/add-employee/:id" component = {CreateEmployeeComponent}></Route>
                           <Route path = "/view-employee/:id" component = {ViewEmployeeComponent}></Route> */}
                           {/* <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route> */}
+
+
+                          // this is a comment
