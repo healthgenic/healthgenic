973c48acf6711d6503fbc9730e8ed1111d9f71b1
Ajay <ajayshinde1@protonmail.com>
2022-02-09 20:06:38 +0530

    Implement UserServiceInterface

---
+++ back-end/src/main/java/com/healthgenic/entities/user/service/UserService.java
@@ -0,0 +1,48 @@
+package com.healthgenic.entities.user.service;
+
+import java.util.Optional;
+
+import org.hibernate.HibernateException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.healthgenic.entities.response.Response;
+import com.healthgenic.entities.user.dao.UserDaoInterface;
+import com.healthgenic.entities.user.model.User;
+
+public class UserService implements UserServiceInterface {
+	@Autowired 
+	private UserDaoInterface userDaoInterface;
+	
+	@Override
+	public User getUser(int id) {
+		Optional<User> userX = null;
+		User user = null;
+		try {
+			userX = userDaoInterface.findById(id);
+		}catch(IllegalArgumentException iae) {
+			return null;
+		}
+		if(userX.isPresent()) {
+			user = userX.get();
+		}
+		return user;
+
+	}
+
+	@Override
+	public Response createUser(User user) {
+		User userFromDatabase = null;
+		try {
+			userFromDatabase = userDaoInterface.save(user);
+		}catch(HibernateException he) {
+			System.out.println("Exception occured while saving user "
+					+ "object into database");
+			String errorPageUrl = "http://localhost:8080/error-page";
+			return new Response(errorPageUrl);
+		}
+		String userHomePageUrl = "http://localhost:8080/user/"+userFromDatabase.getId();
+		System.out.println(userHomePageUrl);
+		return new Response(userHomePageUrl);
+	}
+
+}
