10cfe94440f573cd77e8c8ea9450ac26471ae45f
Ajay Shinde <ajayshinde1@protonmail.com>
2022-02-12 20:49:33 +0530

    Add message field in Response entity

--- back-end/src/main/java/com/healthgenic/entities/response/Response.java
+++ back-end/src/main/java/com/healthgenic/entities/response/Response.java
@@ -2,10 +2,12 @@ package com.healthgenic.entities.response;
 
 public class Response {
 	private String urlToRedirect;
-	
-	public Response(String urlToRedirect) {
+	private String message;
+
+	public Response(String urlToRedirect, String message) {
 		super();
 		this.urlToRedirect = urlToRedirect;
+		this.message = message;
 	}
 
 	public String getUrlToRedirect() {
@@ -15,5 +17,12 @@ public class Response {
 	public void setUrlToRedirect(String urlToRedirect) {
 		this.urlToRedirect = urlToRedirect;
 	}
-	
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
 }
