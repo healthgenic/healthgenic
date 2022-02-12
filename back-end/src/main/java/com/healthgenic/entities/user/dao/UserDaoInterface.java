048422fe48253afc8e81a3efe6e40d2d4e9d2670
Ajay Shinde <ajayshinde1@protonmail.com>
2022-02-12 20:53:47 +0530

    Define a query to find row by mobile number

--- back-end/src/main/java/com/healthgenic/entities/user/dao/UserDaoInterface.java
+++ back-end/src/main/java/com/healthgenic/entities/user/dao/UserDaoInterface.java
@@ -3,7 +3,9 @@ package com.healthgenic.entities.user.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.healthgenic.entities.user.model.User;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserDaoInterface extends JpaRepository<User, Integer> {
-
+    @Query(value = "SELECT * FROM user WHERE mobile_number = ?1", nativeQuery = true)
+    User findByMobileNumber(Long mobileNumber);
 }
