878925d33aab9f013fd9b54fdc52a7a60cb78bf5
Ajay Shinde <ajayshinde1@protonmail.com>
2022-02-14 21:56:52 +0530

    Modify UserLogin.jsx
    Send login details from this page to /login route

--- front-end/src/Components/Login/UserLogin.jsx
+++ front-end/src/Components/Login/UserLogin.jsx
@@ -1,47 +1,77 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 // import './login.scss';
-
+import axios  from 'axios';
 export default class UserLogin extends Component {
+  onSignInSubmit = (e) => {
+    e.preventDefault()
+    // this is a temporary TEST code
+    let user ={};
+      let userName= document.getElementById("form2").value;
+      const emailRe = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      const mobileNumberRe = /^[0-9]{10}$/;
+      if(userName.match(emailRe)){
+        user.email = userName;
+      }else if(userName.match(mobileNumberRe)){
+        user.mobileNumber = userName;
+      }
+      user.password = document.getElementById("password").value;
+      console.log(user);
+      axios.post('http://localhost:8080/login', user)
+      .then(function (response) {
+        let serverResponseData = response.data;
+        console.log(JSON.stringify(serverResponseData));
+
+        if(serverResponseData != ""){
+          document.body.innerHTML = `<code>${JSON.stringify(serverResponseData)}</code>`;
+        }else{
+          document.body.innerHTML = `<code>Error :No user found. ${JSON.stringify(serverResponseData)}</code>`;
+        } 
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+  }
   render() {
     return (
       <div>
-        <form method="post" action="loginVerification.jsp">
+        <form method="GET" action="localhost:8080/user" >
           <h3>
             <b>Login to your account</b>
           </h3>
 
-          <div class="form-outline mb-2">
-            <label class="form-label" for="form2Example11">
+          <div className="form-outline mb-2">
+            <label className="form-label" htmlFor="form2Example11">
               Username
             </label>
             <input
               type="email"
               id="form2"
-              class="form-control"
+              className="form-control"
               placeholder="Phone number or email address"
             />
           </div>
 
-          <div class="form-outline mb-2">
-            <label class="form-label" for="form2">
+          <div className="form-outline mb-2">
+            <label className="form-label" htmlFor="form2">
               Password
             </label>
-            <input type="password" id="form2" class="form-control" />
+            <input type="password" id="password" className="form-control" />
           </div>
 
-          <div class="text-center pt-1 mb-2 pb-1">
-            <button class="btn btn-success btn-block fa-lg mb-3" type="button">
+          <div className="text-center pt-1 mb-2 pb-1">
+            <button className="btn btn-success btn-block fa-lg mb-3" type="button" onClick={this.onSignInSubmit}>
               Log in
             </button>
-            <a class="text-muted" href="#!">
+            <a className="text-muted" href="#!">
               Forgot password?
             </a>
           </div>
 
-          <div class="d-flex align-items-center justify-content-center pb-2">
-            <p class="mb-2 me-2">Don't have an account?</p>
-            <button type="button" class="btn btn-outline-danger">
+          <div className="d-flex align-items-center justify-content-center pb-2">
+            <p className="mb-2 me-2">Don't have an account?</p>
+            <button type="button" className="btn btn-outline-danger">
              <Link to="/DocRegister">Create new</Link> 
             </button>
           </div>
