33127a8fe48cdee2ae785d89a331b07498728702
Ajay Shinde <ajayshinde1@protonmail.com>
2022-02-14 15:15:24 +0530

    change route of getUser

--- back-end/src/main/java/com/healthgenic/entities/user/controller/UserController.java
+++ back-end/src/main/java/com/healthgenic/entities/user/controller/UserController.java
@@ -1,16 +1,10 @@
 package com.healthgenic.entities.user.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.healthgenic.entities.response.Response;
 import com.healthgenic.entities.user.model.User;
 import com.healthgenic.entities.user.service.UserServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -35,7 +29,7 @@ public class UserController {
 		return new Response(redirectionUrl,message);
 
 	}
-	@GetMapping("/user/{id}")
+	@GetMapping("/user/")
 	public User getUser(@PathVariable int id) {
 		return userServiceInterface.getUser(id);
 	}
