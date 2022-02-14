5c85399b378e1519f898fd6e6fe3c7a3de4523e3
Ajay Shinde <ajayshinde1@protonmail.com>
2022-02-14 21:52:27 +0530

    Create Query to retrieve user from db using mobile number or email

--- back-end/src/main/java/com/healthgenic/entities/user/dao/UserDaoInterface.java
+++ back-end/src/main/java/com/healthgenic/entities/user/dao/UserDaoInterface.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 public interface UserDaoInterface extends JpaRepository<User, Integer> {
     @Query(value = "SELECT * FROM user WHERE mobile_number = ?1", nativeQuery = true)
     User findByMobileNumber(Long mobileNumber);
+    @Query(value = "SELECT * FROM user WHERE mobile_number = ?1 OR email = ?2", nativeQuery = true)
+    User findByMobileNumberAndEmail(Long mobileNumber, String email);
 }
