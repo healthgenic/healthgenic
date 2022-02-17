97d0713373950d83116ad7c72d1cf566a175611b
Akshay Agrawal <aks.agrawal275@gmail.com>
2022-02-17 21:42:32 +0530

    implement login and register

---
+++ back-end/src/main/java/com/healthgenic/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.healthgenic.repository;
+
+import com.healthgenic.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+}
