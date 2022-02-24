636c9f1d78e0fab0fef79bfce637f53bd699b124
Mahesh Divate <divatemahesh11@gmail.com>
2022-02-24 09:43:06 +0530

    video player

--- front-end/src/Components/Online_Consultation/VideoPlayer.jsx
+++ front-end/src/Components/Online_Consultation/VideoPlayer.jsx
@@ -1,32 +1,71 @@
-import React, { useContext, useRef } from 'react'
-import { SocketContext } from '../../SocketContext';
+import React, { useContext } from 'react';
 import { Grid, Typography, Paper, makeStyles } from '@material-ui/core';
 
+import { SocketContext } from '../../SocketContext';
+
+const useStyles = makeStyles((theme) => ({
+  video: {
+    width: '550px',
+    [theme.breakpoints.down('xs')]: {
+      width: '300px',
+    },
+  },
+  gridContainer: {
+    justifyContent: 'center',
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+  paper: {
+    padding: '10px',
+    border: '2px solid black',
+    margin: '10px',
+  },
+}));
 
 const VideoPlayer=()=> {
 
     const {name,callAccepted,myVideo,userVideo,callEnded,stream,call} =useContext(SocketContext);
+    const classes = useStyles();
   return (
-      <div className='container'>
-        <div className='card-deck col-6'>
-        {stream && (
-            <div className='card border'>
-                <video playsInline muted ref={myVideo} autoPlay />
-                <button>Call</button>
-            </div>
-        )}
 
-        {callAccepted && !callEnded && (
-            <div className='card border col-6'>
-                <video playsInline ref={userVideo} autoPlay />
-            </div>
-        )}
-        </div>
-        
-    </div>
-  )
-}
+    <Grid container className={classes.gridContainer}>
+    {stream && (
+      <Paper className={classes.paper}>
+        <Grid item xs={12} md={6}>
+          <video playsInline muted ref={myVideo} autoPlay className={classes.video} />
+        </Grid>
+      </Paper>
+    )}
+    {callAccepted && !callEnded && (
+      <Paper className={classes.paper}>
+        <Grid item xs={12} md={6}>
+          <video playsInline ref={userVideo} autoPlay className={classes.video} />
+        </Grid>
+      </Paper>
+    )}
+  </Grid>
+
+  );
+};
 
 export default VideoPlayer;
 
 
+{/* <div className='container'>
+<div className='card-deck col-6'>
+{stream && (
+    <div className='card border'>
+        <video playsInline muted ref={myVideo} autoPlay />
+        <button>Call</button>
+    </div>
+)}
+
+{callAccepted && !callEnded && (
+    <div className='card border col-6'>
+        <video playsInline ref={userVideo} autoPlay />
+    </div>
+)}
+</div>
+
+</div> */}
