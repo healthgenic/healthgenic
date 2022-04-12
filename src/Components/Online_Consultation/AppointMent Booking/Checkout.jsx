833b7634b1d64969e9e7044661257dcf70b31df8
Ajay Shinde <ajayshinde1@protonmail.com>
2022-04-12 13:34:15 +0530

    remove front-end non related code

--- src/Components/Online_Consultation/AppointMent Booking/Checkout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
- 
-const Checkout=()=>
-{
-  return <h1>Welcome to GeeksforGeeks</h1>;
-}
- 
-export default Checkout;
--- src/Components/Online_Consultation/AppointMent Booking/Payment.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Payment extends Component {
-  render() {
-    return (
-      <div>Redirecting to payment Page</div>
-    )
-  }
-}
