1dd42ba5139998111f2896e7c175bcbfb3db7217
Ajay Shinde <ajayshinde1@protonmail.com>
2022-04-12 13:44:16 +0530

    Create Dropdown in navbar for logged in user

--- front-end/src/Components/HomePage/SharedContent/Header.jsx
+++ front-end/src/Components/HomePage/SharedContent/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
-import AuthService from '../../../service/auth.service'
+import AuthService from "../../../service/auth.service";
 
 function Header() {
   const [currentUser, setCurrentUser] = useState(undefined);
@@ -21,10 +21,13 @@ function Header() {
   const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
   return (
     <div className="container-fluid bg-light" id="nav-container">
-      <nav className="container nav-element-container d-flex justify-content-between align-items-center" >
+      <nav className="container nav-element-container d-flex justify-content-between align-items-center">
         <h3 className="text-success">
           <i className="fas fa-heartbeat me-1"></i>
-         <Link to="/" className="text-success text-decoration-none"> HealthGenic</Link>
+          <Link to="/" className="text-success text-decoration-none">
+            {" "}
+            HealthGenic
+          </Link>
         </h3>
 
         <ul className="nav-list-container d-flex align-items-center">
@@ -34,11 +37,18 @@ function Header() {
             </Link>
           </li>
 
-          {!equals(currentUser?.roles, ['ROLE_DOCTOR']) ? (<li className="nav-element ms-3">
-            <Link className="navbar-link px-3 py-2 rounded" to="/Video_Consult">
-              Book Appointment
-            </Link>
-          </li>) : (<></>) }
+          {!equals(currentUser?.roles, ["ROLE_DOCTOR"]) ? (
+            <li className="nav-element ms-3">
+              <Link
+                className="navbar-link px-3 py-2 rounded"
+                to="/Video_Consult"
+              >
+                Book Appointment
+              </Link>
+            </li>
+          ) : (
+            <></>
+          )}
 
           <li className="nav-element ms-3">
             <Link className="navbar-link px-3 py-2 rounded" to="/Epharma">
@@ -52,16 +62,19 @@ function Header() {
             </Link>
           </li>
 
-          {equals(currentUser?.roles, ['ROLE_DOCTOR']) ? <li className="nav-element ms-3">
-            <Link
-              className="navbar-link px-3 py-2 rounded"
-              aria-current="page"
-              to="/consult"
-            >
-              Video Consultation
-            </Link>
-          </li> : <></>}
-
+          {equals(currentUser?.roles, ["ROLE_DOCTOR"]) ? (
+            <li className="nav-element ms-3">
+              <Link
+                className="navbar-link px-3 py-2 rounded"
+                aria-current="page"
+                to="/consult"
+              >
+                Video Consultation
+              </Link>
+            </li>
+          ) : (
+            <></>
+          )}
 
           {/* <li className="navbar-link px-3 py-2 rounded">
               <Link className="navbar-link" to="/Register">
@@ -74,31 +87,36 @@ function Header() {
               </Link> 
     </li>*/}
 
-
           {currentUser ? (
-            <div className="navbar-nav ml-auto">
-              <li className="nav-item">
-              {!equals(currentUser?.roles, ['ROLE_DOCTOR']) ? <Link to={"/profile"} className="nav-link">
-                  {currentUser.username}
-                </Link>:<Link to={"/docdetails"} className="nav-link">
-                  {currentUser.username}
-                </Link>}
-              </li>
-              <li className="nav-item">
-                <a href="/login" className="nav-link" onClick={logOut}>
+              <li class="nav-item dropdown">
+              
+              <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              {currentUser.username}
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li> {!equals(currentUser?.roles, ["ROLE_DOCTOR"]) ? (
+                  <Link to={"/profile"} className="nav-link text-dark">
+                    Profile
+                  </Link>
+                ) : (
+                  <Link to={"/docdetails"} className="nav-link text-dark">
+                   Profile
+                  </Link>
+                )}</li>
+                <li> <a href="/login" className="nav-link text-dark" onClick={logOut}>
                   LogOut
-                </a>
-              </li>
-            </div>
+                </a></li>
+               
+              </ul>
+            </li>
+      
           ) : (
             <div>
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item ms-3">
-             
-                    <Link className="btn  btn-outline-success" to="/login">
-                      Login/Register
-                    </Link>
-          
+                  <Link className="btn  btn-outline-success" to="/login">
+                    Login/Register
+                  </Link>
                 </li>
               </ul>
             </div>
