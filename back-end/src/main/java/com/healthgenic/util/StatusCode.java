c1293440d252225625eadfb413d61d427e339224
Ajay Shinde <ajayshinde1@protonmail.com>
2022-04-12 14:14:33 +0530

    Move front end code to root dir

--- back-end/src/main/java/com/healthgenic/util/StatusCode.java
+++
@@ -1,8 +0,0 @@
-package com.healthgenic.util;
-
-public class StatusCode {
-
-    public final static int USERNAME_ALREADY_EXIST = 901;
-    public final static int ACCESS_TOKEN_EXPIRED = 902;
-    public static final int ACCESS_TOKEN_VALID = 903;
-}
